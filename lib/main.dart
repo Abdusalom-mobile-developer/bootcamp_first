9b0329f48ed3b895729162ce5397a6a485065083
Abdusalom <abdusalom@gmail.com>
2024-08-05 22:10:00 +0500

    chnaged the fontweight of the text

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,9 @@ void main() {
               textAlign: TextAlign.center,
               "Dart is a programming language designed by Lars Bak and Kasper Lund and developed by Google. It can be used to develop web and mobile apps as well as server and desktop applications. Dart is an object-oriented, class-based, garbage-collected language with C-style syntax. It can compile to machine code, JavaScript, or WebAssembly. It supports interfaces, mixins, abstract classes, reified generics and type inference.",
               style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18,
-              ),
+                  color: Colors.blue,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
             ),
           ],
         ),
