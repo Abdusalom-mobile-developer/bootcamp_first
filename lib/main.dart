2b87b265166fd5b6d0a69fdddb668ecee8df6c6a
Abdusalom <abdusalom@gmail.com>
2024-08-05 22:01:38 +0500

    added all needed things to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MaterialApp(
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Column(
-        children: [
-          Image(image: NetworkImage()),
-          Text("Dart"),
-          Text("Dart is a programming language designed by Lars Bak and Kasper Lund and developed by Google.[8] It can be used to develop web and mobile apps as well as server and desktop applications. Dart is an object-oriented, class-based, garbage-collected language with C-style syntax.[9] It can compile to machine code, JavaScript, or WebAssembly. It supports interfaces, mixins, abstract classes, reified generics and type inference."),
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage("assets/images/dart_icon.png")),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Dart",
+              style: TextStyle(
+                  fontSize: 48,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              "Dart is a programming language designed by Lars Bak and Kasper Lund and developed by Google. It can be used to develop web and mobile apps as well as server and desktop applications. Dart is an object-oriented, class-based, garbage-collected language with C-style syntax. It can compile to machine code, JavaScript, or WebAssembly. It supports interfaces, mixins, abstract classes, reified generics and type inference.",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
       ),
     ),
   ));
 }
-
-
